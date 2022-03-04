894d762cc3abaebc4a3209b1d707aa0408ee736b
Aze Bilir <azebiliraze@gmail.com>
2022-03-05 02:37:52 +0300

    adding react icons and deleting task buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import Header from "./components/Header";
+import Tasks from "./Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appoinment",
+      day: "March 7th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "March 8th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "March 8th at 3:30pm",
+      reminder: false,
+    },
+  ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
     <div className="className">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Task To Show"
+      )}
     </div>
   );
 }
--- src/Tasks.js
+++ src/Tasks.js
@@ -1,26 +1,19 @@
 import React from "react";
+import Task from "./components/Task";
 
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appoinment",
-    day: "March 7th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meeting at School",
-    day: "March 8th at 1:30pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Food Shopping",
-    day: "March 8th at 3:30pm",
-    reminder: false,
-  },
-];
-
-export const Tasks = () => {
-  return <div>Tasks</div>;
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task
+          key={tasks.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
+      ))}
+    </>
+  );
 };
+
+export default Tasks;

863bb0169ac0a09f75bbf5929aca541da688db76
Aze Bilir <azebiliraze@gmail.com>
2022-03-05 00:11:45 +0300

    adding tasks component

---
+++ src/Tasks.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const tasks = [
+  {
+    id: 1,
+    text: "Doctors Appoinment",
+    day: "March 7th at 2:30pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Meeting at School",
+    day: "March 8th at 1:30pm",
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Food Shopping",
+    day: "March 8th at 3:30pm",
+    reminder: false,
+  },
+];
+
+export const Tasks = () => {
+  return <div>Tasks</div>;
+};
